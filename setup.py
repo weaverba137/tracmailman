f495177b34c1febf474446d8e6504e41d0215dcf
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-07-28 15:36:24 -0700

    update name

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-PACKAGE = 'tracmailman'
+
+PACKAGE = 'TracMailman'
 VERSION = '1.0.0.dev1234'
 SUMMARY = 'A Trac plugin integrating searching of Mailman archives'
 AUTHOR = 'Theron Ji, Spencer Fang, Benjamin Weaver'
@@ -8,11 +9,10 @@ EMAIL = 'benjamin.weaver@noirlab.edu'
 
 
 setup(name=PACKAGE,
-    version=VERSION,
-    description=SUMMARY,
-    author=AUTHOR,
-    author_email=EMAIL,
-    packages=['tracmailman'],
-    package_data={PACKAGE : ['templates/*.html']},
-    entry_points={'trac.plugins': 'tracmailman.web_ui = tracmailman.web_ui'},
-)
+      version=VERSION,
+      description=SUMMARY,
+      author=AUTHOR,
+      author_email=EMAIL,
+      packages=['tracmailman'],
+      package_data={PACKAGE : ['templates/*.html']},
+      entry_points={'trac.plugins': 'tracmailman.web_ui = tracmailman.web_ui'},)
